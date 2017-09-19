31c11b85c6e2adcb34c8353cfd5076bd8e33b291
TheOwnlySockPuppet <the_sock_puppet@hotmail.com>
2017-09-19 15:53:37 -0700

    Updated the animation controller and added new animations.

---
+++ Assets/Animations/Animations_BrianGideon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script works with the animation controller for the player character Brian Gideon.
+public class Animations_BrianGideon : MonoBehaviour
+{
+    Animator animator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+        //animator.SetBool("isGrinding", true);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
