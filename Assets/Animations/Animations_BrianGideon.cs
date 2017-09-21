9bc3cb4b1ac0d58646cb6d099e71c843eb53761c
TheOwnlySockPuppet <neillaguardia@gmail.com>
2017-09-21 16:11:42 -0700

    Updated animator controller and its script .

--- Assets/Animations/Animations_BrianGideon.cs
+++ Assets/Animations/Animations_BrianGideon.cs
@@ -23,21 +23,48 @@ public class Animations_BrianGideon : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-
+        // Idle
+        if (player.grounded == true && player.current_speed == 0)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+        }
+        // isWalking
+        else if (player.grounded == true && player.current_speed > 0 && player.current_speed < 5)
+        {
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isWalking", true);
+        }
+        // isRunning
+        else if (player.grounded == true && player.current_speed >= 5)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", true);
+        }
         // isJumping
-        if ((Input.GetButton("Controller_A")) && player.jump_counter < player.jump_limit)
+        if ((Input.GetButton("Controller_A")))
         {
             animator.SetBool("isJumping", true);
         }
-        // inTheAir
+        // (Not in the air!)
         if (player.grounded == true)
         {
+            animator.SetBool("isJumping", false);
             animator.SetBool("inTheAir", false);
         }
+        // inTheAir
         else if (player.grounded == false)
         {
-            animator.SetBool("isJumping", false);
             animator.SetBool("inTheAir", true);
+        }
+        // Air Dash
+        if(player.dashing == true)
+        {
+            animator.SetBool("isAirDashing", true);
+        }
+        else if (player.dashing == false)
+        {
+            animator.SetBool("isAirDashing", false);
         }
     }
 }
