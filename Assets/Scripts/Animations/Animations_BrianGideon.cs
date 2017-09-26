d8b54b9bd8c762812734952588f8ab5334bca7da
TheOwnlySockPuppet <neillaguardia@gmail.com>
2017-09-26 16:59:38 -0700

    Updated more of the animation controller.

--- Assets/Scripts/Animations/Animations_BrianGideon.cs
+++ Assets/Scripts/Animations/Animations_BrianGideon.cs
@@ -10,56 +10,72 @@ public class Animations_BrianGideon : MonoBehaviour
     public GameObject playerObject;
     public PlayerGamepad player;
 
+    float maxWalkSpeed;
+
 	// Use this for initialization
 	void Start ()
     {
-        animator = GetComponent<Animator>();
-        //animator.SetBool("isGrinding", true);
+        animator = GetComponent<Animator>();    // Here we can refer to the animator controller we need to use.
+        player = playerObject.GetComponent<PlayerGamepad>();    // We will also need certain variables from the gamepad script.
 
-        player = playerObject.GetComponent<PlayerGamepad>();
-
+        maxWalkSpeed = 10F;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        // isStationary
+        // Most of these animation conditions use the boolean parameters set up in the animation controller.
+
+        // Idle (neither walking nor running, not there is no Idle parameter in the animation controller)
         if (player.grounded == true && player.current_speed == 0)
         {
             animator.SetBool("isRunning", false);
             animator.SetBool("isWalking", false);
         }
         // isWalking
-        else if (player.grounded == true && player.current_speed > 0 && player.current_speed <= 10)
+        else if (player.grounded == true && player.current_speed > 0 && player.current_speed <= maxWalkSpeed)
         {
             animator.SetBool("isRunning", false);
             animator.SetBool("isWalking", true);
         }
         // isRunning
-        else if (player.grounded == true && player.current_speed > 10)
+        else if (player.grounded == true && player.current_speed > maxWalkSpeed)
         {
             animator.SetBool("isWalking", false);
             animator.SetBool("isRunning", true);
         }
+
         // isJumping
-        if ((Input.GetButton("Controller_A")))
+        if ((Input.GetButton("Controller_A") && player.grounded == true))
         {
-            animator.SetBool("isJumping", true);
+            animator.SetBool("isJumping", true); 
         }
+
         // Landed!
         if (player.grounded == true)
         {
-            animator.SetBool("isJumping", false);
             animator.SetBool("inTheAir", false);
         }
-        // inTheAir
+        // inTheAir (not grounded)
         else if (player.grounded == false)
         {
             animator.SetBool("inTheAir", true);
+            animator.SetBool("isJumping", false);   // The player is done jumping the moment his feet leaves the ground. 
         }
 
-        //
-        if(player.grinding == true)
+        // Air Dashing!
+        if(player.dashing == true)
+        {
+            animator.SetBool("isAirDashing", true);
+        }
+        // (not) Air Dashing!
+        else if (player.dashing == false)
+        {
+            animator.SetBool("isAirDashing", false);
+        }
+
+        // Rail Grinding
+        if (player.grinding == true)
         {
             animator.SetBool("isGrinding", true);
         }
