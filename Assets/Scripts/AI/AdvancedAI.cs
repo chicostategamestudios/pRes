edf7ba2fbf7482afbb4ab01c8d3fbd410c891c28
jamest209 <james.tran209@gmail.com>
2017-09-28 15:54:49 -0700

    stagger threshold and advanced AI

---
+++ Assets/Scripts/AI/AdvancedAI.cs
@@ -0,0 +1,128 @@
+﻿/*This script was written by James | Last edited by James | Modified on September 28, 2017
+ *This script piggybacks off of the Basic AI script but with extra functionality.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvancedAI : BasicAI
+{
+    [Header("Advanced stuff")]
+    [Tooltip("Used to keep track of how many attacks have been done in a chain.")]
+    public int current_attack_chain;
+    [Tooltip("The cooldown timer after the AI has finished his 3 chain combo.")]
+    public float attack_chain_cooldown_time = 1f;
+    [Tooltip("This is the cooldown inbetween the chain attacks.")]
+    public float original_attack_chain_cd_time = 0.2f;
+    [Tooltip("The least amount of damage before the AI gets staggered.")]
+    public int stagger_threshold = 20;
+    [Tooltip("The sum of received damage. When this number surpasses the stagger threshold, it will stagger the AI.")]
+    public int total_damage_received = 0;
+    private bool reset_time = false;
+
+    protected IEnumerator Dodge() //this is the dodge of the AI, it will randomly choose left or right dodges.
+    {
+        alerted = false; //set alert to false to make the AI stop chasing the player to allow dodging.
+        current_attack_chain = 0;
+        dodge = (dodge_direction)Random.Range(0, 2); //randomly picks a direction to dodge. 0 is left, 1 is right.
+        yield return new WaitForSeconds(dodge_time);
+        alerted = true; //set alert back to true to make the AI chase the player again.
+        dodge = dodge_direction.not_dodging; //reset the dodge's direction to no direction otherwise known as not_dodging.
+        yield return new WaitForSeconds(cooldown_action);
+        performing_action = false;  //the AI is done with the action
+    }
+    protected IEnumerator Attack_1() //this is the right swing attack. starts from the left arm position
+    {
+        attack_script.check_attack_left = true; //this will turn on the attack script swinging to the left.
+        current_attack_chain += 1;
+        if(current_attack_chain >= 3)
+        {
+            cooldown_action = attack_chain_cooldown_time;
+            current_attack_chain = 0;
+            reset_time = true;
+        }
+        yield return new WaitForSeconds(performing_time);
+        //waits for performing time to be done... then add more things here if needed for after action
+        yield return new WaitForSeconds(cooldown_action);
+        if(current_attack_chain == 0 && reset_time)
+        {
+            reset_time = false;
+            cooldown_action = original_attack_chain_cd_time;
+        }
+        //this is the time to wait for the next action to be performed.
+        performing_action = false;  //the AI is done with the action
+    }
+
+    protected IEnumerator Attack_2() //this is the left swing attack. starts from the right arm position
+    {
+        attack_script.check_attack_right = true; //this will turn on the attack script swinging to the right.
+        current_attack_chain += 1;
+        if (current_attack_chain >= 3)
+        {
+            cooldown_action = attack_chain_cooldown_time;
+            current_attack_chain = 0;
+            reset_time = true;
+        }
+        yield return new WaitForSeconds(performing_time);
+        //waits for performing time to be done... then add more things here if needed for after action
+        yield return new WaitForSeconds(cooldown_action);
+        if (current_attack_chain == 0 && reset_time)
+        {
+            reset_time = false;
+            cooldown_action = original_attack_chain_cd_time;
+        }
+        //this is the time to wait for the next action to be performed.
+        performing_action = false;  //the AI is done with the action
+    }
+
+    protected IEnumerator Attack_3() //this is the diagonal swing attack. stops from the top right arm position 
+    {
+        attack_script.check_attack_top = true; //this will turn on the attack script swinging diagonally.
+        current_attack_chain += 1;
+        if (current_attack_chain >= 3)
+        {
+            cooldown_action = attack_chain_cooldown_time;
+            current_attack_chain = 0;
+            reset_time = true;
+        }
+        yield return new WaitForSeconds(performing_time);
+        //waits for performing time to be done... then add more things here if needed for after action
+        yield return new WaitForSeconds(cooldown_action);
+        if (current_attack_chain == 0 && reset_time)
+        {
+            reset_time = false;
+            cooldown_action = original_attack_chain_cd_time;
+        }
+        //this is the time to wait for the next action to be performed.
+        performing_action = false;  //the AI is done with the action
+    }
+
+    public IEnumerator DamageEnemy(int incoming_damage) //first will apply damage, and then stagger the enemy for a certain duration
+    {
+
+        //apply damage and checks if the enemy dies from the damage.
+        enemy_health -= incoming_damage;
+        total_damage_received += incoming_damage;
+        if(total_damage_received >= stagger_threshold)
+        {
+            //reset the total damage received so it doesn't get stunned repeatedly afterwards being hit.
+            total_damage_received = 0;
+            //set the bools to allow knockback and prevent actions/movements.
+            getting_knockback = true;
+            //attack script's staggered is set to true to uninstantiate any attacks that are already created or about to be instantiated.
+            attack_script.staggered = true;
+            //change the AI state to staggered for animations.
+            current_state = ai_state.staggered;
+            //set staggering to true to affect fixedupdate to prevent the ai from doing any actions.
+            staggering = true;
+            //set alerted to false to stop the AI from following the player with the navmesh.
+            alerted = false;
+            //stop the ai from moving by setting the acceleration, speed, and velocity to 0.
+            transform.GetComponent<UnityEngine.AI.NavMeshAgent>().acceleration = 0;
+            transform.GetComponent<UnityEngine.AI.NavMeshAgent>().speed = 0;
+            transform.GetComponent<UnityEngine.AI.NavMeshAgent>().velocity = Vector3.zero;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
--- Assets/Scripts/AI/BasicAI.cs
+++ Assets/Scripts/AI/BasicAI.cs
@@ -1,8 +1,9 @@
-﻿//This script was written by James | Last edited by James | Modified on September 5, 2017
-//The purpose of this script is to have an enemy chase the player. This is implemented by using a Nav Mesh Agent.
-//It will then decide what to do with its list of actions.
+﻿/*This script was written by James | Last edited by James | Modified on September 26, 2017
+ *The purpose of this script is to have an enemy chase the player. This is implemented by using a Nav Mesh Agent.
+ *It will then decide what to do with its list of actions.
+ *
 
-/* The way this script works is that it requires a nav mesh agent to follow the player. The ai will use the nav mesh when it is "alerted" 
+ * The way this script works is that it requires a nav mesh agent to follow the player. The ai will use the nav mesh when it is "alerted" 
  * so if you don't want the ai to follow the player anymore set "alerted" to false. Once the AI is within a certain distance
  * to the player, it will randomly decide whether to attack or dodge. If it attacks, it will access the BasicAI_Attack script.
  */
@@ -37,14 +38,17 @@ public class BasicAI : MonoBehaviour
     [Header("Timers for actions")]
     public float performing_time = 0.5f; //this is how long the action takes to perform.
 
-    public float dodge_time = 1f; //the amount of time it takes to dodge.
+    public float dodge_time = 0.5f; //the amount of time it takes to dodge.
 
-    public float cooldown_action = 0.5f; //how long the AI will take before it performs another action.
+    public float cooldown_action = 1f; //how long the AI will take before it performs another action.
 
     public float turn_speed = 1f; //how fast the AI can rotate to look at the player.
-
-    public float stagger_duration = 0.5f; //how long the AI is staggered for when they are hit.
-    public float current_stagger_dur = 0f; //the current time of being staggered.
+    [Tooltip("The duration of the AI getting knocked back.")]
+    public float knockback_duration = 0.2f; //how long the AI is staggered for when they are hit.
+    private float current_stagger_dur = 0f; //the current time of being staggered.
+    [Tooltip("The duration of staggering once the knockback is done.")]
+    public float stagger_after_knockback_dur = 0.3f; //the duration of a stun after the knockback.
+    protected bool getting_knockback; //used to stop the knockback once knockback_duration is done.
 
     public float death_duration = 1f;  //the amount of time after the ai reaches 0 hp to be destroyed.
 
@@ -53,37 +57,42 @@ public class BasicAI : MonoBehaviour
                                       //to chase the player or when to attack/dodge.
 
     [Header("Attributes")]
+    [Tooltip("The maximum speed of the enemy's movement.")]
     public float movement_speed = 15;
-    public float acceleration = 100;
+    [Tooltip("The rate of acceleration of the enemy. Higher number is faster time to max movement speed.")]
+    public float acceleration = 50;
+    [Tooltip("This is used for testing. Check this to damage the enemy.")]
     public bool check_to_damage = false; //used for testing, check this to apply damage to the enemy.
 
     private bool first_alert = false; //used to keep track if the AI has been alerted the first time.
-
+    [HideInInspector]
     public bool alerted = false; //once the AI has been alerted, it will start chasing the player.
 
-    private bool performing_action = false; //this is to keep track if the AI is performing an action.
+    protected bool performing_action = false; //this is to keep track if the AI is performing an action.
 
-    private bool staggering = false; //used to set the AI into the staggered state
-
+    protected bool staggering = false; //used to set the AI into the staggered state
+    [Tooltip("The health of the AI. It will die when it is 0.")]
     public int enemy_health = 100; //the health of the enemy.
+    [Tooltip("The force that pushes back the AI when it is hit.")]
+    public float knockback_force = 18f; 
 
     [HideInInspector]
     public int incoming_damage = 0; //the damage that will be applied to the enemy.
 
-    private dodge_direction dodge = dodge_direction.not_dodging; //AI will decide to move left or right in the dodge.
+    protected dodge_direction dodge = dodge_direction.not_dodging; //AI will decide to move left or right in the dodge.
 
     private Quaternion look_rotation; //used to rotate the direction the AI is looking at when the player is nearby.
 
     private Vector3 direction; //also used for the rotation of where the AI is looking at when the player is nearby.
 
+    
 
     [Header("Don't touch!")]
-    public GameObject left_dodge;
-    public GameObject right_dodge; //used to move to the game object when left or right dodging.
+    //used to move to the game object when left or right dodging.
+    public GameObject left_dodge; 
+    public GameObject right_dodge; 
 
-    public Transform knockback_dir; //used for knockback when the AI gets hit.
-
-    private BasicAI_Attack attack_script; //to access its attack script.
+    protected BasicAI_Attack attack_script; //to access its attack script for AI attacks.
 
     private PlayerAttack player_attack; //for the AI to receive damage from the player's attacks.
 
@@ -91,28 +100,27 @@ public class BasicAI : MonoBehaviour
     [HideInInspector]
     public GameObject basic_ai; //the weapon spawner that is in charge of the weapon swinging.
 
-    private Vector3 backward_dir;
-    public float knockback_force = 5f;
+    private Vector3 backward_dir; //the backward_dir of the AI. Used whenever the AI needs to get knocked back.
+    
 
-    ai_state current_state = ai_state.idle; //instantiates the ai with an idle state.
+    protected ai_state current_state = ai_state.idle; //instantiates the ai with an idle state.
 
 
 
-    public IEnumerator Dodge() //this is the dodge of the AI, it will randomly choose left or right dodges.
+    protected IEnumerator Dodge() //this is the dodge of the AI, it will randomly choose left or right dodges.
     {
         alerted = false; //set alert to false to make the AI stop chasing the player to allow dodging.
         dodge = (dodge_direction)Random.Range(0, 2); //randomly picks a direction to dodge. 0 is left, 1 is right.
 		yield return new WaitForSeconds(dodge_time); 
         alerted = true; //set alert back to true to make the AI chase the player again.
-        dodge = dodge_direction.not_dodging;
+        dodge = dodge_direction.not_dodging; //reset the dodge's direction to no direction otherwise known as not_dodging.
 		yield return new WaitForSeconds(cooldown_action);
 		performing_action = false;  //the AI is done with the action
     }
 
-    public IEnumerator Attack_1() //this is the right swing attack. starts from the left arm position
+    protected IEnumerator Attack_1() //this is the right swing attack. starts from the left arm position
     {
         attack_script.check_attack_left = true; //this will turn on the attack script swinging to the left.
-        
         yield return new WaitForSeconds(performing_time);
         //waits for performing time to be done... then add more things here if needed for after action
 		yield return new WaitForSeconds(cooldown_action);
@@ -120,10 +128,9 @@ public class BasicAI : MonoBehaviour
 		performing_action = false;  //the AI is done with the action
     }
 
-    public IEnumerator Attack_2() //this is the left swing attack. starts from the right arm position
+    protected IEnumerator Attack_2() //this is the left swing attack. starts from the right arm position
     {
         attack_script.check_attack_right = true; //this will turn on the attack script swinging to the right.
-        
         yield return new WaitForSeconds(performing_time);
         //waits for performing time to be done... then add more things here if needed for after action
         yield return new WaitForSeconds(cooldown_action);
@@ -131,10 +138,9 @@ public class BasicAI : MonoBehaviour
         performing_action = false;  //the AI is done with the action
     }
 
-    public IEnumerator Attack_3() //this is the diagonal swing attack. stops from the top right arm position 
+    protected IEnumerator Attack_3() //this is the diagonal swing attack. stops from the top right arm position 
     {
         attack_script.check_attack_top = true; //this will turn on the attack script swinging diagonally.
-        
         yield return new WaitForSeconds(performing_time);
         //waits for performing time to be done... then add more things here if needed for after action
         yield return new WaitForSeconds(cooldown_action);
@@ -145,12 +151,18 @@ public class BasicAI : MonoBehaviour
     //set up for the AI attacks and movement.
     private void Awake()
     {
+        //find the backward direction for knockbacks.
         backward_dir = transform.TransformDirection(Vector3.back);
-        basic_ai = this.transform.FindChild("WeaponSpawn").gameObject; //finds the child object to access its script for attacks.
-        attack_script = basic_ai.GetComponent<BasicAI_Attack>();  //allows access to the attack script.
-        target = GameObject.Find("Player").transform; //sets the target of this AI as the player
-        transform.GetComponent<UnityEngine.AI.NavMeshAgent>().acceleration = acceleration; //applies the acceleration variable of this to the nav mesh agent.
-        transform.GetComponent<UnityEngine.AI.NavMeshAgent>().speed = movement_speed; //applies the speed variable to the nav mesh agent.
+        //find the child object to access its script for attacks.
+        basic_ai = this.transform.FindChild("WeaponSpawn").gameObject;
+        //access to the attack script.
+        attack_script = basic_ai.GetComponent<BasicAI_Attack>();
+        //sets the target of this AI as the player
+        target = GameObject.Find("Player").transform;
+        //apply the acceleration variable of this to the nav mesh agent.
+        transform.GetComponent<UnityEngine.AI.NavMeshAgent>().acceleration = acceleration;
+        //apply the speed variable to the nav mesh agent.
+        transform.GetComponent<UnityEngine.AI.NavMeshAgent>().speed = movement_speed; 
     }
 
 	public IEnumerator DamageEnemy(int incoming_damage) //first will apply damage, and then stagger the enemy for a certain duration
@@ -158,13 +170,15 @@ public class BasicAI : MonoBehaviour
         
         //apply damage and checks if the enemy dies from the damage.
         enemy_health -= incoming_damage;
+        //set the bools to allow knockback and prevent actions/movements.
+        getting_knockback = true;
+        //attack script's staggered is set to true to uninstantiate any attacks that are already created or about to be instantiated.
         attack_script.staggered = true;
-        current_state = ai_state.staggered;
-        alerted = false;
-		staggering = true;
+        //change the AI state to staggered for animations.
         current_state = ai_state.staggered;
         //set staggering to true to affect fixedupdate to prevent the ai from doing any actions.
 		staggering = true;
+        //set alerted to false to stop the AI from following the player with the navmesh.
         alerted = false;
         //stop the ai from moving by setting the acceleration, speed, and velocity to 0.
         transform.GetComponent<UnityEngine.AI.NavMeshAgent>().acceleration = 0;
@@ -185,23 +199,32 @@ public class BasicAI : MonoBehaviour
 		Debug.Log (transform.name);
 		enemy_health = 100;
 		first_alert = false;
-
 	}
 
     void FixedUpdate () 
 	{
-
+        //check first if the enemy is staggered.
         if (staggering)
         {
             current_stagger_dur += Time.deltaTime;
             //move the ai state to staggered, and set alerted to false to stop their movement.
             current_state = ai_state.staggered;
             alerted = false;
+
             //get knocked backwards.
-            transform.Translate(backward_dir * knockback_force * Time.smoothDeltaTime, Space.Self);
+            if (getting_knockback)
+            {
+                transform.Translate(backward_dir * knockback_force * Time.smoothDeltaTime, Space.Self);
+            }
+
+            //once the current_stagger_dur is done with knockback, stop the knockback by turning off getting_knockback.
+            if (current_stagger_dur >= knockback_duration && getting_knockback)
+            {
+                getting_knockback = false;
+            }
 
             //once the stagger duration is up, restore all of the old values of the AI to move and attack.
-            if(current_stagger_dur >= stagger_duration)
+            if (current_stagger_dur >= knockback_duration + stagger_after_knockback_dur)
             {
                 transform.GetComponent<UnityEngine.AI.NavMeshAgent>().acceleration = acceleration;
                 transform.GetComponent<UnityEngine.AI.NavMeshAgent>().speed = movement_speed;
@@ -212,18 +235,19 @@ public class BasicAI : MonoBehaviour
                 attack_script.staggered = false;
             }
         }
+        //it is not staggering so run through the usual routines.
         else
         {
             distance_to_player = Vector3.Distance(target.position, transform.position); //calculate distance to player
 
-            //used for checking purposes. if the enemy is damaged then it must get stunned
+            //used for checking and testing purposes.
             if (check_to_damage)
             {
-                StartCoroutine("DamageEnemy", 1);
+                StartCoroutine("DamageEnemy", 11);
                 check_to_damage = false;
             }
 
-            
+
             if (distance_to_player < 30 && !first_alert) //if the player is close enough, this will set the AI to be alerted. 
                                                          //if the enemy is alerted then it will chase the player. AKA aggro range
             {
@@ -231,14 +255,11 @@ public class BasicAI : MonoBehaviour
                 first_alert = true;
             }
 
-        //if the ai is staggered, then dont do anything for this frame and get knocked back.
-            //move the ai state to staggered, and set alerted to false to stop their movement.
-
-            
+            //if the enemy reached 0 hp, it is dead so it will be put into the dying state then go through the death coroutine.
             if (enemy_health <= 0)
             {
-               current_state = ai_state.dying;
-               StartCoroutine("Death");
+                current_state = ai_state.dying;
+                StartCoroutine("Death");
             }
 
 
@@ -252,37 +273,37 @@ public class BasicAI : MonoBehaviour
 
             if (alerted)  //if the AI is aggro, then it will chase the player.
             {
-                //telling this object to chase after the target's position.
-                transform.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = target.position; 
+                //telling this object to chase after the target's position, otherwise known as the player's position.
+                transform.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = target.position;
             }
 
 
             //if the ai is close enough and not already performing an action
             //then it will pick a random action to do
             //and then perform the selected action.
-            if (distance_to_player <= 5.5f && !performing_action) 
-		    {
+            if (distance_to_player <= 5.5f && !performing_action)
+            {
                 performing_action = true;  //the AI is now doing an action, used to make sure it is only doing one action.
                 //will pick from dodge through attack 3
-                ai_state current_state = (ai_state)Random.Range(2, 6);  
-			    switch (current_state) //based on the choice, do the corresponding coroutines
-			    {
-			    case ai_state.dodging:
-				    StartCoroutine ("Dodge");
-				    break;
-			    case ai_state.attack_1:
-				    StartCoroutine ("Attack_1");
-				    break;
-			    case ai_state.attack_2:
-				    StartCoroutine ("Attack_2");
-				    break;
-                case ai_state.attack_3:
-                    StartCoroutine("Attack_3");
-                    break;
+                ai_state current_state = (ai_state)Random.Range(2, 6);
+                switch (current_state) //based on the choice, do the corresponding coroutines
+                {
+                    case ai_state.dodging:
+                        StartCoroutine("Dodge");
+                        break;
+                    case ai_state.attack_1:
+                        StartCoroutine("Attack_1");
+                        break;
+                    case ai_state.attack_2:
+                        StartCoroutine("Attack_2");
+                        break;
+                    case ai_state.attack_3:
+                        StartCoroutine("Attack_3");
+                        break;
                 }// at the end of each coroutines, it will set performing_action back to false to allow for a loop if the ai is still within range.
-		    }
+            }
 
-
+            // the dodge works by moving towards transform positions of empty game objects that are attached to the ai.
             if (dodge == dodge_direction.left) //this will make the ai move to the left or right when they are in that dodge state.
             {
                 transform.position = Vector3.Lerp(transform.position, left_dodge.transform.position, 1f * Time.deltaTime);
@@ -291,10 +312,7 @@ public class BasicAI : MonoBehaviour
             {
                 transform.position = Vector3.Lerp(transform.position, right_dodge.transform.position, 1f * Time.deltaTime);
             }
-                // the dodge works by moving towards transform positions of empty game objects that are attached to the ai.
-
-
-
+            
         }
     }
 
