c1238f57c812d98abbd79006f43afac3e70d0639
brando1901 <brandooo1901@gmail.com>
2017-09-28 16:02:11 -0700

    bleed screen effect

---
+++ Assets/Scripts/PanelScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelScript : MonoBehaviour {
+
+
+    public Image bleedImage;
+    public float FadeRate;
+    private float targetAlpha = 2f;
+
+    // Use this for initialization
+    void Start () {
+        bleedImage = GameObject.Find("BleedPanel").GetComponent<Image>();
+        bleedImage.color = Color.red;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //var tempColor = bleedImage.color;
+        //tempColor.a++;
+        //bleedImage.color = tempColor;
+
+    }
+}
