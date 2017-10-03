5b2ca6bf254433d00c46f8fe1904d4c81f06e892
TheOwnlySockPuppet <neillaguardia@gmail.com>
2017-10-03 14:25:23 -0700

    Updated playercontroller a bit to have some animations down.

---
+++ Assets/Scripts/Player/Combat_New.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat_New : MonoBehaviour
+{
+    Animator playerAnimator;
+    private PlayerGamepad my_gamepad;
+    public int comboChain;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerAnimator = GetComponent<Animator>();
+        my_gamepad = GetComponent<PlayerGamepad>();
+        comboChain = 0;
+}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Controller_Y"))
+        {
+            comboChain++;
+            playerAnimator.SetInteger("attackCombo", comboChain);
+
+            if(comboChain >= 4)
+            {
+                comboChain = 0;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
